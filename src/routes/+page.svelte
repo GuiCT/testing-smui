756ed9ed4cae5875d3334c6bcbe81ddea3354adb
GuiCT <gtomiasi@gmail.com>
2023-04-09 19:18:30 -0300

    adding checkbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Select any link to a showcase</h1>
 <ul>
   <li><a href='./button'>Button</a></li>
+  <li><a href='./checkbox'>Checkbox</a></li>
 </ul>
---
+++ src/routes/checkbox/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Checkbox from "@smui/checkbox";
+  let status = false;
+  $: statusAsString = status ? "checked" : "unchecked";
+
+  const events = ["First event", "Second event", "Third event"];
+  let selectedEvents: string[] = [];
+  $: selectedEventsStr = selectedEvents.length
+    ? selectedEvents.join(", ") + ` (${selectedEvents.length})`
+    : "No event selected (0)";
+  $: spanColor = status ? "#0F0" : "#F00";
+</script>
+
+<div style="display: flex; flex-direction: row; align-items:center;">
+  <Checkbox bind:checked={status} />
+  <p>
+    The checkbox is <span style="color: {spanColor}; font-weight: bold;"
+      >{statusAsString}</span
+    >
+  </p>
+</div>
+<hr />
+{#each events as event}
+  <div style="display: flex; flex-direction: row; align-items: center;">
+    <Checkbox bind:group={selectedEvents} value={event} />
+    <span>{event}</span>
+  </div>
+{/each}
+<p>{selectedEventsStr}</p>
