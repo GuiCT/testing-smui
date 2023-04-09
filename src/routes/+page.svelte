26ffaa9225e0020bdb7a2a78488208a531a6cb64
GuiCT <gtomiasi@gmail.com>
2023-04-09 18:40:15 -0300

    adding Button component and initializing SMUI

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Select any link to a showcase</h1>
+<ul>
+  <li><a href='./button'>Button</a></li>
+</ul>
---
+++ src/routes/button/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Button from "@smui/button";
+  const variants = [
+    "text",
+    "raised",
+    "unelevated",
+    "outlined",
+  ] as ("text" | "raised" | "unelevated" | "outlined")[];
+  const colors = [
+    "primary",
+    "secondary",
+  ] as ("primary" | "secondary")[];
+</script>
+
+<h1>Buttons</h1>
+{#each colors as color}
+  <h2>Color: {color}
+    {#each variants as variant}
+      <Button variant={variant} color={color}>
+        {variant} {color}
+      </Button>
+    {/each}
+  </h2>
+{/each}
